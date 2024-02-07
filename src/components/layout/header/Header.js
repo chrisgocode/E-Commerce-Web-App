01d74271b3af6f71122f062742f0e928eb601093
Christian <chrisgo@bu.edu>
2024-02-07 15:44:51 -0500

    created header

---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../../assets/images/Amazon_logo.svg';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+function Header() {
+  return (
+    <div className="header">
+      <img src={logo} alt="Amazon logo" className="header__logo" />
+
+      <div className="header__navOption">
+        <span className="header__navOptionLineOne">Delivering to</span>
+        <span className="header__navOptionLineTwo">Boston 02215</span>
+      </div>
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello, sign in</span>
+          <span className="header__optionLineTwo">Account & Lists</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingCartIcon />
+          <span className="header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default Header;
---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Home.css'; // Import the CSS file for styling (create this file in the same directory)
+
+const Home = () => {
+  return <div className="home">Hello World Again!!</div>;
+};
+
+export default Home;
